1ad4801fe229592ec8492ee17e983bb304d9833f
Vanessa <vanessaabordo@yahoo.com>
2018-07-15 14:28:24 +0800

    done stencil and bootstrap sass integration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
 
 import '@ui-guys/stencil-bs-ui-lib';
+import { UiLibraryComponent } from './ui-library/ui-library.component';
+import { ExampleFormComponent } from './example-form/example-form.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  { path: 'ui-library', component: UiLibraryComponent },
+  { path: 'example-form', component: ExampleFormComponent },
+  { path: '', component: HomeComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UiLibraryComponent,
+    ExampleFormComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(routes),
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent],
