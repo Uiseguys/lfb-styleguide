bdccf6c31265d1fd12aa700f421c93f0282b6ed1
Vanessa <vanessaabordo@yahoo.com>
2018-07-16 20:54:44 +0800

    remove definecustomelements

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,12 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { defineCustomElements } from '@ui-guys/stencil-bs-ui-lib';
+// import { defineCustomElements } from '@ui-guys/stencil-bs-ui-lib';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
-} else {
-  defineCustomElements(window);
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
