6aee02d862bd0b5ddd7afa28bfb715a9a447be94
gafilipoie <marius.afilipoie@gmail.com>
2018-07-22 22:14:09 +0300

    integrated form-generator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,8 @@ import {Component} from '@angular/core';
     styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+    schema: any
+    form: any
     title = 'app';
     multiSelectData: any = ['Herr Müller', 'Frau Förster'];
     names: any = [
@@ -208,18 +210,172 @@ export class AppComponent {
         'Stafford, Shoshana N.'
         ,
         'Vega, Suki L.'
-        ];
-        isClassActive = true;
-        getSearchText() {
-          if (this.isClassActive) {
-            return 'Search';
+    ];
+
+    isClassActive = true;
+
+    getSearchText() {
+      if (this.isClassActive) {
+        return 'Search';
+      }
+      return 'Close';
+    }
+
+    getTitleText() {
+      if (this.isClassActive) {
+        return 'Meine Buchungen';
+      }
+      return 'Suche';
+    }
+
+    constructor() {
+        this.schema = {
+          "type": "object",
+          "required": ["checkbox", "min", "max", "startDate", "endDate", "startDateString", "endDateString", "dropdown", "autocomplete"],
+          "properties": {
+            "autocomplete": {
+              "$id": "data/properties/autocomplete",
+              "type": "array",
+              "arrayType": "autocomplete",
+              "items": {
+                "$id": "/properties/autocomplete/items",
+                "type": "object",
+                "placeholder": "Search something e.g. 'Argentina'",
+                "searchKey": "data.name",
+                "data": [
+                  {
+                    "type": 'country',
+                    "data": {
+                      "name": 'Austria',
+                      "capital": 'Vienna'
+                    }
+                  },
+                  {
+                    "type": 'country',
+                    "data": {
+                      "name": 'Australia',
+                      "capital": 'Canberra'
+                    }
+                  },
+                  {
+                    "type": 'country',
+                    "data": {
+                      "name": 'Argentina',
+                      "capital": 'Buenos Aires'
+                    }
+                  }
+                ]
+              }
+            },
+            "checkbox": {
+              "$id": "data/properties/checkbox",
+              "type": "boolean",
+              "description": "An explanation about the purpose of this instance.",
+              "placeholder": "Check Me",
+              "default": false,
+              "examples": [
+                false
+              ]
+            },
+            "duration": {
+              "$id": "data/properties/duration",
+              "type": "object",
+              "properties": {
+                "min": {
+                  "$id": "data/properties/duration/properties/min",
+                  "type": "integer",
+                  "description": "An explanation about the purpose of this instance.",
+                  "placeholder": "Min Value",
+                  "default": 0,
+                  "examples": [
+                    5
+                  ]
+                },
+                "max": {
+                  "$id": "data/properties/duration/properties/max",
+                  "type": "integer",
+                  "description": "An explanation about the purpose of this instance.",
+                  "placeholder": "Max Value",
+                  "default": 0,
+                  "examples": [
+                    10
+                  ]
+                }
+              }
+            },
+            "date": {
+              "$id": "data/properties/date",
+              "type": "object",
+              "format": "date",
+              "placeholder": "Select a Date",
+              "startDate": {
+                "$id": "data/properties/date/startDate",
+                "type": "string"
+              },
+              "endDate": {
+                "$id": "data/properties/date/endDate",
+                "type": "string"
+              },
+              "lang": {
+                "$id": "data/properties/date/lang",
+                "type": "string"
+              },
+              "dateFormat": {
+                "$id": "data/properties/date/dateFormat",
+                "type": "string"
+              }
+            },
+            "startDateString": {
+              "$id": "data/properties/startDateString",
+              "type": "string",
+              "description": "An explanation about the purpose of this instance.",
+              "placeholder": "Start Date",
+              "default": "",
+              "examples": [
+                "2007-08-31T16:47+00:00"
+              ]
+            },
+            "endDateString": {
+              "$id": "data/properties/endDateString",
+              "type": "string",
+              "description": "An explanation about the purpose of this instance.",
+              "placeholder": "End Date",
+              "default": "",
+              "examples": [
+                "2007-08-31T16:47+00:00"
+              ]
+            },
+            "dropdown": {
+              "$id": "data/properties/dropdown",
+              "type": "array",
+              "arrayType": "dropdown",
+              "items": {
+                "$id": "/properties/dropdown/items",
+                "type": "string",
+                "buttonText": "Select",
+                "placeholder": "Select a value",
+                "enum": ["Automatic", "Manual"]
+              }
+            }
           }
-          return 'Close';
-        }
-        getTitleText() {
-          if (this.isClassActive) {
-            return 'Meine Buchungen';
-          }
-          return 'Suche';
-        }
+        };
+
+        this.form = {
+          "autocomplete": [],
+          "checkbox": false,
+          "duration": {
+            "min": 5,
+            "max": 10
+          },
+          "date": {
+            "startDate": "03/25/2018",
+            "endDate": "04/12/2018",
+            "lang": "en",
+            "dateFormat": "MM.DD.YYYY"
+          },
+          "startDateString": "2007-08-31T16:47+00:00",
+          "endDateString": "2007-08-31T16:47+00:00",
+          "dropdown": []
+        };
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import '@ui-guys/stencil-bs-ui-lib/dist/ui-lib';
+import '@ui-guys/form-generator/dist/form-generator';
 
 @NgModule({
   declarations: [
