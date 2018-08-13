e9cfd4e1deb477c4be0ae4f8af48814a5e0b6602
AlexanderSergan <jfdi686@gmail.com>
2018-08-13 05:49:36 +0300

    partial workaround

--- src/app/views/styleguide/styleguide.component.ts
+++ src/app/views/styleguide/styleguide.component.ts
@@ -6,5 +6,18 @@ import {Component} from '@angular/core';
     styleUrls: ['./styleguide.component.scss']
 })
 export class StyleguideComponent {
+constructor() {
 
+}
+
+names: any[] = [
+    'Baker, Nero X.'
+    ,
+    'Nguyen, Isabelle L.'
+    ,
+    'Forbes, Ayanna O.'
+    ,
+    'Mcgee, Abra I.'
+    ,
+]
 }
